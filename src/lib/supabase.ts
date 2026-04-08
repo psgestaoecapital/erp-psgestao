c5926dc1a1307a4f85c0668f234b6947a722307b
PS Gestao Deploy <deploy@psgestao.com.br>
2026-04-08 10:36:53 +0000

    Staging: deploy inicial

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,3 +5,4 @@ const supabaseUrl = 'https://horsymhsinqcimflrtjo.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvcnN5bWhzaW5xY2ltZmxydGpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyODE0MjYsImV4cCI6MjA5MDg1NzQyNn0.s2GbtX69F0HtH_uhbBt3cnV8opXPJEdDQlolkhir1Mo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// staging 1775644613
